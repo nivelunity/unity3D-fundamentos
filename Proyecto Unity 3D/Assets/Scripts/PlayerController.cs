cfd8f72440789b450fb85f746488835cf53b195d
Gaston Caminiti <gastoncaminiti@gmail.com>
2023-10-22 19:46:39 -0300

    Interfaz

--- Proyecto Unity 3D/Assets/Scripts/PlayerController.cs
+++ Proyecto Unity 3D/Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,15 +10,26 @@ public class PlayerController : MonoBehaviour
     [Range(1f, 25f)]
     private float speed = 10f;
 
+    [SerializeField]
+    private TextMeshProUGUI countText;
+
+    [SerializeField]
+    private GameObject winTextObject;
+
     private Rigidbody rb;
 
     private float movementX;
     private float movementY;
 
+    private int count;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winTextObject.SetActive(false);
     }
 
     void OnMove(InputValue movementValue)
@@ -38,6 +50,17 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("Coleccionable"))
         {
             other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+            if (count >= 12)
+            {
+                winTextObject.SetActive(true);
+            }
         }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Gemas: " + count.ToString();
     }
 }
