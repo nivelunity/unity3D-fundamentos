db7fa13c6323941284301acafb16b81592ebb75e
Gaston Caminiti <gastoncaminiti@gmail.com>
2023-12-10 17:53:03 -0300

    VFX Granja

--- Proyecto Unity 3D/Assets/Scripts/Farm.cs
+++ Proyecto Unity 3D/Assets/Scripts/Farm.cs
@@ -3,43 +3,91 @@ using UnityEngine;
 
 public class Farm : MonoBehaviour
 {
-    [SerializeField]
-    [Range(10, 200)]
-    int foodPoints = 100;
-
+ 
     [SerializeField]
     ParticleSystem lootSFX;
 
+    [SerializeField]
+    Renderer floorRenderer;
+
+    [SerializeField]
+    Material lootMaterial;
+
+    [SerializeField]
+    [Range(1, 9)]
+    int foodPoints = 9;
+
+    [SerializeField]
+    [Range(9, 18)]
+    int lootTolalTime = 10;
+
     private bool isLooting = false;
+    private Material originalMaterial;
+    private WaitForSeconds waitLoot;
+    private int foodIndex = 0;
+
+    private void OnEnable()
+    {
+        originalMaterial = floorRenderer.material;
+        waitLoot = new WaitForSeconds(lootTolalTime/foodPoints);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player") && !IsFarmEmpty())
         {
             isLooting = true;
-            lootSFX.Play();
+            LootingFarm();
             StartCoroutine(LootingCorrutine());
         }
     }
 
+    private void LootingFarm()
+    {
+        lootSFX.Play();
+        floorRenderer.material = lootMaterial;
+    }
+
     private void OnTriggerExit(Collider other)
     {
+        if (IsFarmEmpty()) return;
+
         if (other.gameObject.CompareTag("Player"))
         {
             isLooting = false;
-            lootSFX.Stop();
+            CancelLooting();
             StartCoroutine(LootingCorrutine());
         }
+    }
+
+    private void CancelLooting()
+    {
+        floorRenderer.material = originalMaterial;
+        lootSFX.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
     }
 
     IEnumerator LootingCorrutine()
     {
         while (isLooting)
         {
-            yield return new WaitForSeconds(1f);
-            foodPoints--;
-        }
+            yield return waitLoot;
 
-        yield return null;
+            if (!isLooting) break;
+            foodPoints--;
+            transform.GetChild(foodIndex).gameObject.SetActive(false);
+            foodIndex++;
+
+            if (IsFarmEmpty())
+            {
+                isLooting = false;
+                CancelLooting();
+                Debug.Log("GRANJA LOOTEADA");
+            }
+        }
+    }
+
+    private bool IsFarmEmpty()
+    {
+        return foodPoints == 0;
     }
 }
