50f2dd26f76d2fb24f2ca5a74711b69f6d7391ff
Gaston Caminiti <gastoncaminiti@gmail.com>
2023-12-11 09:52:13 -0300

    Enemy Radio Deteccion

--- Proyecto Unity 3D/Assets/Scripts/EnemyController.cs
+++ Proyecto Unity 3D/Assets/Scripts/EnemyController.cs
@@ -8,6 +8,14 @@ public class EnemyController : MonoBehaviour
     [SerializeField]
     private Transform jugador;
 
+    [SerializeField]
+    [Range(2f,15f)]
+    private float radioDeteccion = 10f;
+
+    [SerializeField]
+    [Range(45f, 180f)]
+    private float fieldOfView = 90f;
+
     private NavMeshAgent navMeshAgent;
     // Start is called before the first frame update
     void Start()
@@ -18,9 +26,23 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (jugador != null)
+        if (jugador == null) return;
+
+        float distance = Vector3.Distance(transform.position, jugador.position);
+
+
+        Vector3 toPlayer = (jugador.position - transform.position).normalized;
+        float angleToPlayer = Vector3.Angle(transform.forward, toPlayer);
+
+        if (distance <= radioDeteccion && (angleToPlayer <= fieldOfView * 0.5f))
         {
             navMeshAgent.SetDestination(jugador.position);
         }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, radioDeteccion);
     }
 }
