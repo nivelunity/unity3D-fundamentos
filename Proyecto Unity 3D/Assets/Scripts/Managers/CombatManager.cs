f28fb51aa44f9d3e94c3a46e84a61da1a94a2ac4
Gaston Caminiti <gastoncaminiti@gmail.com>
2023-12-14 11:43:27 -0300

    Match Flow

--- Proyecto Unity 3D/Assets/Scripts/Managers/CombatManager.cs
+++ Proyecto Unity 3D/Assets/Scripts/Managers/CombatManager.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class CombatManager : MonoBehaviour
 {
+    [SerializeField]
+    [Range(1,5)]
+    private int maxMatches = 3;
+
     private enum Choice
     {
         Rock,
@@ -21,12 +25,29 @@ public class CombatManager : MonoBehaviour
     private Choice playerChoice;
     private Choice enemyChoice;
 
+    private int played = 0;
+    private MatchState[] matches;
+
+    private void Start()
+    {
+        matches = new MatchState[maxMatches];
+    }
+
     public void PlayerChoice(int newChoice)
     {
+        if (played == maxMatches) return;
+
         playerChoice = (Choice)newChoice;
         enemyChoice  = (Choice)Random.Range(0, 3);
+
+        matches[played] = Match();
+       
         Debug.Log(playerChoice);
         Debug.Log(enemyChoice);
+        Debug.Log(matches[played]);
+
+        played++;
+
     }
 
     private MatchState Match()
